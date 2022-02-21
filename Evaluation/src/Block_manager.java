a80589070711a3aee4e0c0b0e686290959fe78d3
zrock-dev <santisaavcon@gmail.com>
2022-02-21 17:43:23 -0400

    print status ok

--- Evaluation/src/Block_manager.java
+++ Evaluation/src/Block_manager.java
@@ -13,25 +13,26 @@ public class Block_manager {
 
     void fillList(){
         //red block
-        double[] rSize = {40.6, 34.9};
+        double[] rSize = {40.6, 34.9, 6};
         blocks.put("R", rSize);
 
         //orange block
-        double[] oSize = {28.15, 19.6};
+        double[] oSize = {28.15, 19.6, 3};
         blocks.put("O", oSize);
 
         //yellow block
-        double[] ySize = {20.9, 15.5};
+        double[] ySize = {20.9, 15.5, 3};
         blocks.put("Y", ySize);
 
         //green block
-        double[] gSize = {28.0, 20.0};
+        double[] gSize = {28.0, 20.0, 3};
         blocks.put("G", gSize);
 
         //blue block
-        double[] bSize = {32.6, 24.0};
+        double[] bSize = {32.6, 24.0, 4};
         blocks.put("B", bSize);
     }
+
     void showBlockInfo(){
         for (int i = 0; i <= 4; i++) {
             // Title
@@ -40,6 +41,7 @@ public class Block_manager {
             double height = blocks.get(colorCode)[0];
             double width = blocks.get(colorCode)[1];
             String surface  = String.format("%.2f", height*width);
+            int parcelQuantity = (int) blocks.get(colorCode)[2];
 
             System.out.println("=".repeat(lineChar));
             System.out.println(" ".repeat(21) + name + " BLOCK");
@@ -48,11 +50,11 @@ public class Block_manager {
             // Details
             System.out.println("   CODE" + "\t\t" + "HEIGHT[m]" + "\t\t" + "WIDTH[m]" + "\t\t" + "SURFACE[m2]");
             System.out.println("-".repeat(lineChar));
-            System.out.print("|" + colorCode + "-00" + i + "\t\t" );
-            System.out.println("|" + height + "\t\t" + "|" + width + "\t\t" + "|" + surface);
+            for (int parcel = 1; parcel < parcelQuantity; parcel++) {
+                System.out.print("|" + colorCode + "-00" + parcel + "\t\t" );
+                System.out.println("|" + height + "\t\t" + "|" + width + "\t\t" + "|" + surface);
+            }
             System.out.println(" ");
-
-
         }
     }
 
